61f52b8136f998da1df4996fff6d7b07f4488604
Matti Kauppila <makaupp@gmail.com>
2018-10-09 14:17:55 +0300

    Block activation sequencer added

---
+++ Assets/Scripts/Level Components/BlockHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockHandler : MonoBehaviour {
+
+	public bool[] Sequence;
+	public float SecondsPerFrame = 0.5f;
+	public float AnimationDuration = 0.3f;
+	private bool animationDirection;
+	private float animationCounter;
+	private float counter = 0.0f;
+	private int cursor = 0;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+		if(counter > SecondsPerFrame)	
+		{
+			counter = 0;
+			++cursor;
+			if(cursor >= Sequence.Length) cursor = 0;
+			gameObject.SetActive(Sequence[cursor]);
+
+		}
+		counter += Time.deltaTime;
+	}
+}
