83e108f477c0491fd54994a8c58fc3980bad05e0
Frans <xelimra@gmail.com>
2018-11-16 16:55:53 +0200

    Moving Spike blocks, Death animation base

--- Assets/Scripts/TriggerScript.cs
+++ Assets/Scripts/TriggerScript.cs
@@ -6,6 +6,9 @@ public class TriggerScript : MonoBehaviour {
 
 
     private GameObject gameController;
+    public Sprite[] deathPieceSprites;
+    public GameObject deathPiece;
+    public GameObject prefabSpawnPoint;
 
 
     private void Start()
@@ -13,6 +16,18 @@ public class TriggerScript : MonoBehaviour {
         gameController = GameObject.Find("GameController");
     }
     
+
+    private void GenerateDeathPrefabs()
+    {
+        for(int i = 0; i < deathPieceSprites.Length; i++)
+        {
+            GameObject piece = (GameObject)Instantiate(deathPiece, prefabSpawnPoint.gameObject.transform.position, prefabSpawnPoint.gameObject.transform.rotation);
+            piece.gameObject.transform.Rotate(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
+            piece.GetComponent<SpriteRenderer>().sprite = deathPieceSprites[i];
+            piece.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(10f, -10f), Random.Range(-25f, -50f));
+            Destroy(piece, 10f);
+        }
+    }
 
     //Handle all the collisions player does, with spikes and goal for respawning or level change.
     private void OnTriggerEnter2D(Collider2D col)
@@ -22,6 +37,7 @@ public class TriggerScript : MonoBehaviour {
             {
                 
                  GameObject[] clones = GameObject.FindGameObjectsWithTag("Player");
+            GenerateDeathPrefabs();
             foreach(GameObject clone in clones)
             {
               Destroy(clone);
