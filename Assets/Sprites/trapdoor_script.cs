7b4c7c57b1b7960b03904f592e9c0b8dc7ba6af0
Valtteri24 <valtteriirettlav@gmail.com>
2018-11-22 01:10:07 +0200

    Gingerbread man pieces, trapdoor object

---
+++ Assets/Sprites/trapdoor_script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trapdoor_script : MonoBehaviour {
+    
+    public GameObject sphere;
+
+	void disablePlane () {
+		transform.parent.gameObject.GetComponent<MeshCollider>().enabled = false;
+        
+	}
+	void enablePlane () {
+		transform.parent.gameObject.GetComponent<MeshCollider>().enabled = true;
+	}
+}
